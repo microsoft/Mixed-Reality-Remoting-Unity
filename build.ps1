374dfb4c8774d848acf1a414e1142a9f9cd066c4
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2023-07-10 18:07:14 +0100

    Write build info to console

--- build.ps1
+++ build.ps1
@@ -1,10 +1,23 @@
 $unityPath = "C:\Program Files\Unity\Hub\Editor\2020.3.20f1\Editor\Unity.exe"
-$buildPath = [IO.Path]::Combine($PSScriptRoot, 'build')
+
+$outputPath = [IO.Path]::Combine($PSScriptRoot, "build")
+
+$apkProjectPath = [IO.Path]::Combine($PSScriptRoot, "MR-Remoting-Android-App.Unity")
+$upmProjectPath = [IO.Path]::Combine($PSScriptRoot, "MR-Remoting-Host.Unity.Unity")
+
+$apkLogPath = [IO.Path]::Combine($outputPath, "build-apk.log")
+$upmLogPath = [IO.Path]::Combine($outputPath, "build-unity-packages.log")
 
 # Build APK
-$apkBuildArgs = @("-batchmode", "-projectpath ./MR-Remoting-Android-App.Unity", "-executeMethod BuildApk.Build", "-TargetFolder ""$buildPath""", "-logFile build/build-apk.log")
+$apkBuildArgs = @("-batchmode", "-projectpath ""$apkProjectPath""", "-executeMethod BuildApk.Build", "-TargetFolder ""$outputPath""", "-logFile ""$apkLogPath""")
+Write-Output "Building Android companion app with args: "
+Write-Output $apkBuildArgs
 Start-Process -FilePath $unityPath -ArgumentList $apkBuildArgs -Wait
 
+Write-Output ""
+
 #  Build UPM
-$upmBuildArgs = @("-batchmode", "-projectpath ./MR-Remoting-Host.Unity", "-executeMethod MRMobileRemoting.UPMTools.CreatePackage", "-TargetFolder ""$buildPath""", "-logFile build/build-unity-package.log")
+$upmBuildArgs = @("-batchmode", "-projectpath ""$upmProjectPath""", "-executeMethod MRMobileRemoting.UPMTools.CreatePackage", "-TargetFolder ""$outputPath""", "-logFile ""$upmLogPath""")
+Write-Output "Building Unity package with args: "
+Write-Output $upmBuildArgs
 Start-Process -FilePath $unityPath -ArgumentList $upmBuildArgs -Wait
